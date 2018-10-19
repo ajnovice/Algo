31bb9dbd45df72e33fddc513a729c6170956513d
Ajay Singh <ajaysimple91@gmail.com>
2018-10-19 01:09:11 -0500

    Java Array

--- src/Implementation/Implementation.java
+++ src/Implementation/Implementation.java
@@ -1,4 +1,27 @@
 package Implementation;
 
+class Student{
+    public int roll_no;
+    public String name;
+    Student(int roll_no,String name){
+        this.roll_no=roll_no;
+        this.name=name;
+    }
+}
+
 public class Implementation {
+
+    public static  void main(String[] args){
+        Student[] stu = new Student[5];
+        stu[0] = new Student(1,"Ajay1");
+        stu[1] = new Student(2,"Ajay2");
+        stu[2] = new Student(3,"Ajay3");
+        stu[3] = new Student(4,"Ajay4");
+        stu[4] = new Student(5,"Ajay5");
+
+        for(int i =0; i<stu.length;i++ ){
+            System.out.println("Roll No :"+stu[i].roll_no+" Name :"+stu[i].name);
+        }
+
+    }
 }
