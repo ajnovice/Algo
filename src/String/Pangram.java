dc8bdb5856db5d5f942df6b645238a85b13df1f8
Ajay Singh <ajaysimple91@gmail.com>
2018-11-17 14:21:41 -0600

    Pangram

---
+++ src/String/Pangram.java
@@ -0,0 +1,28 @@
+package String;
+
+import java.util.HashSet;
+
+public class Pangram {
+
+    public static void CheckPangram(String s1) {
+        HashSet<Character> set = new HashSet<>();
+        s1 = s1.toLowerCase();
+        for (int i = 0; i < s1.length(); i++) {
+            if ('a' <= s1.charAt(i) && s1.charAt(i) <= 'z') {
+                set.add(s1.charAt(i));
+            }
+        }
+        if (set.size() == 26) {
+            System.out.println("It's a Pangram");
+        } else {
+            System.out.println("It's not a Pangram");
+        }
+    }
+
+    public static void main(String[] args) {
+        String s1 = new String("The quick brown fox jumps over the lazy dog");
+        String s2 = new String("The quick brown fox jumps over the dog");
+        CheckPangram(s1);
+        CheckPangram(s2);
+    }
+}
