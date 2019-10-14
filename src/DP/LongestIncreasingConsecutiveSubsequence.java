41544fe0a0a7e4b86ebafe0186a0601f1e867d76
Ajay Singh <ajaysimple91@gmail.com>
2019-10-13 22:51:29 -0500

    https://www.geeksforgeeks.org/longest-increasing-consecutive-subsequence/ Naive

--- src/DP/LongestIncreasingConsecutiveSubsequence.java
+++ src/DP/LongestIncreasingConsecutiveSubsequence.java
@@ -1,35 +1,46 @@
 package DP;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class LongestIncreasingConsecutiveSubsequence {
 
-    private static int longest(int[] arr){
+    static class Pair{
+        int index;
+        List<Integer> list;
+        Pair(int index,List<Integer> list){
+            this.index=index;
+            this.list=list;
+        }
+    }
+
+    private static int longestNaive(int[] arr){
         int n=arr.length;
-        int[] result = new int[n];
-        Arrays.fill(arr,1);
-        for(int i=1;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(arr[j]<arr[i] && (result[i]<(result[j]+1))){
-                    result[i]=result[j]+1;
+        List<Pair> result = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            int prev=arr[i];
+            List<Integer> x = new ArrayList<>();
+            x.add(arr[i]);
+            result.add(new Pair(i,x));
+            for(int j=i;j<n;j++){
+                if((prev+1)==arr[j]){
+                    result.get(i).list.add(arr[j]);
+                    prev=arr[j];
                 }
             }
         }
-        int max=1;
-        int curr=1;
-        for(int i=0;i<result.length-1;i++){
-            if((result[i]+1)==result[i+1]&&(max<(curr+1))){
-                max=curr+1;
-                curr=max;
-            }else
-                curr=1;
+        int max=0;
+        for(Pair pair:result){
+            if(pair.list.size()>max){
+                max=pair.list.size();
+            }
         }
         return max;
     }
 
     public static void main(String[] args){
-        int a[] = { 3, 10, 3, 11, 4, 5, 6, 7, 8, 12 };
-        System.out.println(longest(a));
+        int a[] = { 6, 7, 8, 3, 4, 5, 9, 10 };
+        System.out.println(longestNaive(a));
     }
 }
---
+++ src/DP/LongestIncreasingSubsequencePrint.java
@@ -0,0 +1,12 @@
+package DP;
+
+public class LongestIncreasingSubsequencePrint {
+
+    private static void printLongest(int arr){
+
+    }
+
+    public static void main(String[] args){
+        System.out.println();
+    }
+}
